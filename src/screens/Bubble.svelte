19d9728cc6788d397a52b8550966bd9985aa0905
LireEruel <jsh001505@gmail.com>
2022-09-06 18:19:39 +0900

    설명 추가 및 예외처리

--- src/screens/Bubble.svelte
+++ src/screens/Bubble.svelte
@@ -9,6 +9,7 @@
   let heightFactor = 5;
   let sppedFactor = 10;
   let targetBar = -1;
+  let isSorting = false;
 
   for (let i = 0; i < numOfBars; i++) {
     targetArray[i] = i + 1;
@@ -27,12 +28,16 @@
     arr[xp] = arr[yp];
     arr[yp] = temp;
   }
-  function sortButtonClick() {
-    bubbleSort();
+  async function sortButtonClick() {
+    if (isSorting) return; // sorting함수가 2개가 동시에 동작하지 않도록 수정
+    isSorting = true;
+    await bubbleSort();
+    isSorting = false;
     targetArray = targetArray;
   }
   async function bubbleSort() {
     var i, j;
+    isSorting = true;
     for (i = 0; i < numOfBars - 1; i++) {
       for (j = 0; j < numOfBars - i - 1; j++) {
         targetBar = j + 1;
@@ -51,6 +56,21 @@
   <h1>bubble</h1>
   <button on:click={sortButtonClick}>sort</button>
   <button on:click={mixButtonClick}>mix</button>
+  <div>
+    <h3>
+      bubble sort는 인접한 요소의 순서가 잘못된 경우 반복적으로 교체하여
+      작동하는 가장 간단한 정렬 알고리즘입니다.
+    </h3>
+    <p>시간 복잡도 : O(N^2 ) 보조 공간: O(1)</p>
+    <p>
+      <a href="https://www.geeksforgeeks.org/bubble-sort/?ref=lbp">참고자료</a>
+    </p>
+  </div>
+  <label>
+    <p>speed</p>
+    <input type="number" bind:value={sppedFactor} min="0.1" max="10" />
+    <input type="range" bind:value={sppedFactor} min="0.1" max="10" />
+  </label>
   <div id="bars_container">
     {#each targetArray as bar, index}
       {#if targetBar === index}
